b44cc3f73b9f0189603bc790f267598d19bef1d7
LeandroArias01 <leanahuelarias4@gmail.com>
2024-08-21 23:38:44 -0300

    empece con el nav (falta terminarlo) y poner el carrusel y las cards mas anchas. y hacer todo el index responsive

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const menuIcon = document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.icon');
+const navLinks = document.querySelector('.nav-links');
+
+menuIcon.addEventListener('click', function() {
+    navLinks.classList.toggle('show');
+    menuIcon.style.display = 'none';
+    closeIcon.style.display = 'block';
+});
+
+closeIcon.addEventListener('click', function() {
+    navLinks.classList.remove('show');
+    closeIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
+});
